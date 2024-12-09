fdae03ec1e3fe2034dd98f1bcc7ec111e5f8895f
Dilshad <helloshad3@gmail.com>
2024-12-09 15:16:05 +0530

    Shadcn UI Setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
