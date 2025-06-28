b8cb55b4609f3ad470f072ce8d5ac7c6364555f1
Dilshad <helloshad3@gmail.com>
2025-06-28 21:41:50 +0530

    changes in navbar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,13 +5,25 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="bg-teal-500  flex justify-between w-full items-center px-4 py-3 border-b border-slate-400">
-      <Link href="/" className="uppercase tracking-wider text-slate-900">
-        FAST REACT PIZZA CO..
+      <Link href="/"  className="text-2xl font-bold tracking-tight text-slate-600 dark:text-blue-400"
+          aria-label="Home"
+          style={{ fontFamily: "monospace" }}>
+      
+          
+      
+          &lt; Pizza /&gt;
+        
       </Link>
-      <div className="">
+      <div className="flex gap-2">
+        <div>
+
         <Searchorder />
-      </div>
+        </div>
+        <div className="my-2">
+
         <Username />
+        </div>
+      </div>
     </header>
   );
 };
